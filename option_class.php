cd9d1cb01d163a3ba8aaeb9de456924ecd54bd2f
fwhcat <fabienwang@free.fr>
2018-05-05 01:41:42 +0200

    Fixed option class thx Romain

--- option_class.php
+++ option_class.php
@@ -1,51 +1,11 @@
 <?php
-
-if( !defined('DB_HOST') ) {
-    include 'filemanager_config.php';
-}
-
-require_once 'filemanager_core.php';
-
 class option_class extends filemanager_core
 {
-    var $db;
-
-    function __construct()
-    {
-        try {
-          $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-          var_dump($this->db);
-        } catch (Exception $exception){
-          return $exception->getMessage();
-        }
-    }
-
-    public function mysql_request($query) {
-        try {
-          $request_response = $this->db->query($query);
-          return $request_response;
-        } catch (Exception $exception){
-          return $exception->getMessage();
-        }
-    }
-
-    public function quote($txt){
-        return $this->db->quote($txt);
-    }
-
-    private function encode_me($txt)
-    {
-        $txt = strip_tags($txt);
-        $txt = $this->quote($txt);
-        $txt = urlencode($txt);
-        return $txt;
-    }
-
     function get_option($name)
     {
         $content = array();
         $select = $this->mysql_request("SELECT * FROM filemanager_options WHERE option_name='$name'");
-        while($row = mysql_fetch_array($select))
+        while($row = $select->fetch())
         {
             $content = $this->decode($row["option_content"]);
         }
@@ -110,7 +70,7 @@ class option_class extends filemanager_core
     private function exists_option($name)
     {
         $select = $this->mysql_request("SELECT id FROM filemanager_options WHERE option_name='$name'");
-        $num = mysql_num_rows($select);
+        $num = $select->rowCount();
         if($num <= 0)
         {
             return false;
